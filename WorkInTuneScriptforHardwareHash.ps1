fd4c62484a901aea4005b96ab71ef44ade33014f
jonathansnyder <46977972+jonathansnyder@users.noreply.github.com>
2019-09-13 16:43:06 -0600

    Deleted irrelavant data

--- WorkInTuneScriptforHardwareHash.ps1
+++
@@ -1,8 +0,0 @@
-
-#script to run the machine hash powershell script
-
-md c:\\HWID
-Set-Location c:\\HWID
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
-Install-Script -Name Get-WindowsAutoPilotInfo
-Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
