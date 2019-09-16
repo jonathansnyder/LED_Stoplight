7ca81abbcee3dee725544c21204ef8e548ab3e98
jonathansnyder <46977972+jonathansnyder@users.noreply.github.com>
2019-09-15 19:52:58 -0600

    Added the logic into the controller

--- stoplightctr.py
+++ stoplightctr.py
@@ -1,9 +1,69 @@
+import RPI.GPIO as GPIO
 from flask import Flask, render_template
+
+
 stoplightctr = Flask(__name__)
+
+#imports rpi library and grabs neccessary parts from flask
+
+
+GPIOControlLogic = Flask(__name__)
+
+#using the board numbers for pins not broadcom
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+#Name the pin numbers
+GreenLed = 11
+YellowLed = 13
+RedLed = 15
+
+#set the stoplight pins to output
+#pin green
+GPIO.output(GreenLed, GPIO.OUT)
+
+#pin yellow
+GPIO.output(YellowLed, GPIO.OUT)
+
+#pin red
+GPIO.output(RedLed, GPIO.OUT)
+
+#initialize all lights off
+GPIO.output(GreenLed, GPIO.LOW)
+GPIO.output(YellowLed, GPIO.LOW)
+GPIO.output(RedLed, GPIO.LOW)
+
 
 @stoplightctr.route('/StoplightControler')
 def StoplightControler():
     return render_template('StoplightControler.html')
+
+@stoplightctr.route("/<option>")
+def action(option):
+    if option == Red:
+        #only red light on
+        GPIO.output(RedLed, GPIO.HIGH)
+        GPIO.output(YellowLed, GPIO.LOW)
+        GPIO.output(GreenLed, GPIO.LOW)
+    if option == Yellow:
+        #only yellow light on
+        GPIO.output(RedLed, GPIO.LOW)
+        GPIO.output(YellowLed, GPIO.HIGH)
+        GPIO.output(GreenLed, GPIO.LOW)
+    if option == Green:
+        #only green light on
+        GPIO.output(RedLed, GPIO.LOW)
+        GPIO.output(YellowLed, GPIO.LOW)
+        GPIO.output(GreenLed, GPIO.HIGH)
+    if option == none:
+        #all lights off
+        GPIO.output(RedLed, GPIO.LOW)
+        GPIO.output(YellowLed, GPIO.LOW)
+        GPIO.output(GreenLed, GPIO.LOW)
+    if option == auto:
+
+
+
 
 if __name__ == "__main__":
     stoplightctr.run()
